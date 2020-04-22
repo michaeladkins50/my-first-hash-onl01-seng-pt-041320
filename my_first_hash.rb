f84e08dac50aa59e22298fd8ba72901b5182eb69
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-22 15:56:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ my_hash = {"name" => "Michael Adkins"}
 end
 
 
-def shipping_manifest
+def shipping_manifest(the_manifest)
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
